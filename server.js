cb40542c88d8548f102d84fe5579f2bb8790132d
helberadrian <helberadrian@gmail.com>
2022-02-08 16:20:11 -0300

    Sumando socket.io
    No funciona la parte de socket

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const app = express();
 const productos = require("./routes/app");
 const morgan = require("morgan");
 const PORT = 3000;
+const almacen = require("./class/class");
+const product = new almacen();
+
+// Websocket
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
 
 // settings
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 app.set("json spaces", 2);
 app.use(require("./routes/app"));
+
+//const { engine } = require("express-handlebars");
 
 // middlewares
 app.use(morgan("dev"));
@@ -14,6 +24,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname, + "./public"));
 app.use("/api", productos);
+
+// Websocket - Server
+const ListaProductos = [];
+
+io.on("connection", (socket) => {
+    socket.on("new_product", (data) =>{
+        product.getAll(ListaProductos);
+        ListaProductos.push(data);
+        io.sockets.emit("product_received", ListaProductos);
+    });
+});
 
 // starting the server
 const server = app.listen(PORT, () =>{
